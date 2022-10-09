06ce55a8a81c48de1a68912dba3b77da236e4f95
shu1007 <shuu.na1007@icloud.com>
2022-10-10 01:39:57 +0900

    Read file at constructor

--- src/utils/db/index.ts
+++ src/utils/db/index.ts
@@ -1,6 +1,37 @@
 import { IDatabase, IDBKeyType } from '../../types/db.types';
+import dotenv from 'dotenv'
+import fs from 'fs'
+import logger from '../logger'
 class SpeedCache<K extends IDBKeyType, T> implements IDatabase<K, T> {
   protected db: Record<K, T> = {} as Record<K, T>;
+
+  constructor() {
+    dotenv.config()
+    const path = process.env.JSON_PATH;
+    if(!path){
+        logger.error(null, 'JSON_PATH was not find.')
+        throw Error()
+    }
+
+    if(fs.existsSync(path)){
+        fs.readFile(path, 'utf-8', (err, data) => {
+            if (err) {
+                logger.error(err, 'File read failed')
+                return;
+            }
+            Object.entries(JSON.parse(data)).forEach(ent  => {
+                this.db[ent[0] as K] = ent[1] as T
+            })
+        })
+    } else {
+        fs.writeFile(path, '{}', 'utf-8', err => {
+            if (err) {
+                logger.error(err, 'Error writing file')
+                throw Error()
+            }
+        })
+    }
+  }
 
   async get(id: K): Promise<T> {
     return this.db[id];
