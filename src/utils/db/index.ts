5b33f5756cce2aabaa6a160d0ca7938cafdc3c36
Kareem Ebrahim <kareemmahlees@gmail.com>
2023-10-21 17:52:18 +0200

    refactor: expiray within the set function

--- src/utils/db/index.ts
+++ src/utils/db/index.ts
@@ -1,85 +1,88 @@
-import { IDatabase, IDBKeyType } from '../../types/db.types';
 import dotenv from 'dotenv'
 import fs from 'fs'
+import { IDatabase, IDBKeyType } from '../../types/db.types'
 import logger from '../logger'
 class SpeedCache<K extends IDBKeyType, T> implements IDatabase<K, T> {
-  protected db: Record<K, T> = {} as Record<K, T>;
-  protected jsonPath: string;
+  protected db: Record<K, T> = {} as Record<K, T>
+  protected jsonPath: string
 
   constructor() {
     dotenv.config()
-    const path = process.env.JSON_PATH;
-    if(!path){
-      logger.error(null, 'JSON_PATH was not find.');
-      throw Error();
+    const path = process.env.JSON_PATH
+    if (!path) {
+      logger.error(null, 'JSON_PATH was not find.')
+      throw Error()
     }
-    this.jsonPath = path;
-    this.readJsonFile();
+    this.jsonPath = path
+    this.readJsonFile()
   }
 
   private readJsonFile() {
-    if(fs.existsSync(this.jsonPath)){
+    if (fs.existsSync(this.jsonPath)) {
       fs.readFile(this.jsonPath, 'utf-8', (err, data) => {
         if (err) {
-          logger.error(err, 'Error reading file');
-          return;
+          logger.error(err, 'Error reading file')
+          return
         }
-        this.setDbEntries(data);
+        this.setDbEntries(data)
       })
     } else {
-      fs.writeFile(this.jsonPath, '{}', 'utf-8', err => {
+      fs.writeFile(this.jsonPath, '{}', 'utf-8', (err) => {
         if (err) {
-          logger.error(err, 'Error writing file');
-          throw Error();
+          logger.error(err, 'Error writing file')
+          throw Error()
         }
-      });
+      })
     }
   }
 
   private setDbEntries(data: string): void {
-    Object.entries(JSON.parse(data)).forEach(ent  => {
-      this.db[ent[0] as K] = ent[1] as T;
-    });
+    Object.entries(JSON.parse(data)).forEach((ent) => {
+      this.db[ent[0] as K] = ent[1] as T
+    })
   }
 
   private writeFile() {
-    fs.writeFile(this.jsonPath, JSON.stringify(this.db), 'utf-8', err => {
+    fs.writeFile(this.jsonPath, JSON.stringify(this.db), 'utf-8', (err) => {
       if (err) {
-        logger.error(err, 'Error writing file');
-        throw Error();
+        logger.error(err, 'Error writing file')
+        throw Error()
       }
-    });
+    })
   }
 
   async get(id: K): Promise<T> {
-    return this.db[id];
+    return this.db[id]
   }
 
-  async set(id: K, value: T): Promise<void> {
-    this.db[id] = value;
+  /**
+   *
+   * @param id key of the record
+   * @param value value of the record
+   * @param expiry expiry time in seconds
+   */
+  async set(id: K, value: T, expiry = 0) {
+    if (expiry !== 0) {
+      setTimeout(async () => {
+        await this.del(id)
+      }, expiry * 1000)
+    }
+    this.db[id] = value
     this.writeFile()
   }
 
-  async setEx(id: K, value: T, expiry: number): Promise<void> {
-    this.set(id, value);
-
-    setTimeout(() => {
-      delete this.db[id];
-    }, expiry);
-  }
-
   async del(id: K): Promise<void> {
-    delete this.db[id];
+    delete this.db[id]
     this.writeFile()
   }
 
   protected async getKeys(): Promise<[string] | undefined> {
-    const keys = Object.keys(this.db);
+    const keys = Object.keys(this.db)
     if (keys.length) {
-      return keys as [string];
+      return keys as [string]
     }
 
-    return undefined;
+    return undefined
   }
 }
 
